c8c0b84821b4c765600e0504704db55bbe7acff0
YOUR NAME <YOUR EMAIL ADDRESS>
2020-09-15 18:25:30 -0700

    Header component added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Header */}
+      <div className="app__body">
+        {/* App Body */}
+        <Sidebar />
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEe6uT3PUbj-9txu5Wek8Yut9fOYrSloQ",
+  authDomain: "facebook-clone-ec66a.firebaseapp.com",
+  databaseURL: "https://facebook-clone-ec66a.firebaseio.com",
+  projectId: "facebook-clone-ec66a",
+  storageBucket: "facebook-clone-ec66a.appspot.com",
+  messagingSenderId: "211357161970",
+  appId: "1:211357161970:web:0a5f7874c25544c63bb36b",
+  measurementId: "G-M3SF6671L8",
+};
